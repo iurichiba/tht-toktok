230591e9f816130a52642f8628a41d5842a088ca
Iuri Chiba <iurichiba@gmail.com>
2022-12-04 20:54:51 -0300

    Initial Commit

---
+++ tht-toktok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tht-toktok
+//
+//  Created by Iuri Chiba on 04/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
